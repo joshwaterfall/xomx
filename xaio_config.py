c282d442ce365e60b2ea6c9fa707a8f50ece4342
perrin-isir <perrin.isir@gmail.com>
2021-03-25 10:20:53 +0100

    Update xaio_config.py

--- xaio_config.py
+++ xaio_config.py
@@ -1,6 +1,6 @@
 import os
 
-CSV_RNASeq_data = "~/Desktop/work/code/RNAseq/data/big.csv"
+CSV_RNASeq_data = "~/Desktop/work/code/RNAseq/data/big.csv" #TPM-normalized data (transcripts per million: sum of transcripts per sample is 1 million)
 CSV_annotations = "~/Desktop/work/code/RNAseq/data/annot_with_fusion_new_names.csv"
 CSV_annot_types = "~/Desktop/work/code/RNAseq/data/annot_types.csv"
 output_dir = os.path.expanduser("~/Desktop/data/xaio")
